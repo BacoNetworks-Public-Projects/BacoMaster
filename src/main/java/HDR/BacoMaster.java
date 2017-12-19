ef46916212d0f3f24fc343dc44ca73497c6893f0
HDR <refshet@gmail.com>
2017-12-19 20:42:09 +0100

    1.2
    Fixes

--- src/main/java/HDR/BacoMaster.java
+++ src/main/java/HDR/BacoMaster.java
@@ -7,7 +7,7 @@ import org.spongepowered.api.plugin.Plugin;
 
 import static HDR.commands.CommandList.*;
 
-@Plugin(id = "bacomaster", name = "BacoMaster", version = "1.1")
+@Plugin(id = "bacomaster", name = "BacoMaster", version = "1.2")
 public class BacoMaster {
 
     private void makeCommands() {
--- src/main/java/HDR/commands/CommandList.java
+++ src/main/java/HDR/commands/CommandList.java
@@ -4,13 +4,13 @@ import org.spongepowered.api.command.spec.CommandSpec;
 import org.spongepowered.api.text.Text;
 
 public class CommandList {
-    private static CommandSpec RefinedCMD = CommandSpec.builder().description(Text.of("Redeem Refined Kit")).permission("master.starterkits").executor(new TeKit()).build();
-    private static CommandSpec EnviroCMD = CommandSpec.builder().description(Text.of("Redeem Enviro Kit")).permission("master.starterkits").executor(new AEKit()).build();
-    private static CommandSpec ThermalCMD = CommandSpec.builder().description(Text.of("Redeem Thermal Kit")).permission("master.starterkits").executor(new ForestryKit()).build();
-    private static CommandSpec XnetCMD = CommandSpec.builder().description(Text.of("Redeem XNet Kit")).permission("master.starterkits").executor(new ImmersiveKit()).build();
-    private static CommandSpec FarmerCMD = CommandSpec.builder().description(Text.of("Redeem Farmer Kit")).permission("master.starterkits").executor(new BotaniaKit()).build();
-    private static CommandSpec EnchanterCMD = CommandSpec.builder().description(Text.of("Redeem Enchanter Kit")).permission("master.starterkits").executor(new TinkersKit()).build();
-    public static CommandSpec StarterCMD = CommandSpec.builder().description(Text.of("")).executor(new StarterCommand()).child(RefinedCMD, "refinedkit").child(EnviroCMD, "envirokit").child(ThermalCMD, "thermalkit").child(XnetCMD,"xnetkit").child(FarmerCMD,"farmerkit").child(EnchanterCMD,"enchanterkit").permission("master.starterkits").build();
+    private static CommandSpec ThermalCMD = CommandSpec.builder().description(Text.of("Redeem Thermal Expansion Kit")).permission("master.starterkits").executor(new TeKit()).build();
+    private static CommandSpec AECMD = CommandSpec.builder().description(Text.of("Redeem AE Kit")).permission("master.starterkits").executor(new AEKit()).build();
+    private static CommandSpec ForestryCMD = CommandSpec.builder().description(Text.of("Redeem Forestry Kit")).permission("master.starterkits").executor(new ForestryKit()).build();
+    private static CommandSpec ImmersiveCMD = CommandSpec.builder().description(Text.of("Redeem Immersive Kit")).permission("master.starterkits").executor(new ImmersiveKit()).build();
+    private static CommandSpec BotaniaCMD = CommandSpec.builder().description(Text.of("Redeem Botania Kit")).permission("master.starterkits").executor(new BotaniaKit()).build();
+    private static CommandSpec TinkersCMD = CommandSpec.builder().description(Text.of("Redeem Tinkers Kit")).permission("master.starterkits").executor(new TinkersKit()).build();
+    public static CommandSpec StarterCMD = CommandSpec.builder().description(Text.of("")).executor(new StarterCommand()).child(ThermalCMD, "thermalkit").child(AECMD, "aekit").child(ForestryCMD, "forestrykit").child(ImmersiveCMD,"immersivekit").child(BotaniaCMD,"botaniakit").child(TinkersCMD,"tinkerskit").permission("master.starterkits").build();
     public static CommandSpec ClearEntitiesCMD = CommandSpec.builder().description(Text.of("")).permission("master.command.entities").executor(new ClearEntitiesCommand()).build();
     public static CommandSpec BacoMasterCMD = CommandSpec.builder().description(Text.of("")).permission("master.command").executor(new BacoMasterCommand()).child(ClearEntitiesCMD, "clear").build();
 }
