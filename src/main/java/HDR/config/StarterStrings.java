131635c8191097a6a71b94ea9ff94ddc983cda1c
HDR <refshet@gmail.com>
2017-12-19 21:20:33 +0100

    1.2 Hotfix 3.0
    damnit

--- src/main/java/HDR/config/StarterStrings.java
+++ src/main/java/HDR/config/StarterStrings.java
@@ -6,11 +6,11 @@ import org.spongepowered.api.text.format.TextColors;
 import org.spongepowered.api.text.format.TextStyles;
 
 public class StarterStrings {
-    public static Text EnviroKitString = Text.builder("Redeem Tinkers Kit").onClick(TextActions.runCommand("/start tinkers")).color(TextColors.AQUA).onHover(TextActions.showText(Text.of("Redeem the Tinkers kit"))).style(TextStyles.BOLD).build();
-    public static Text RefinedKitString = Text.builder("Redeem AE Kit").onClick(TextActions.runCommand("/start ae")).color(TextColors.AQUA).onHover(TextActions.showText(Text.of("Redeem the AE kit"))).style(TextStyles.BOLD).build();
-    public static Text ThermalKitString = Text.builder("Redeem Botania Kit").onClick(TextActions.runCommand("/start botania")).color(TextColors.AQUA).onHover(TextActions.showText(Text.of("Redeem the Botania kit"))).style(TextStyles.BOLD).build();
-    public static Text XnetKitString = Text.builder("Redeem ThermalExpansion Kit").onClick(TextActions.runCommand("/start tex")).color(TextColors.AQUA).onHover(TextActions.showText(Text.of("Redeem the Thermal Expansion kit"))).style(TextStyles.BOLD).build();
-    public static Text FarmerKitString = Text.builder("Redeem Forestry Kit").onClick(TextActions.runCommand("/start forestry")).color(TextColors.AQUA).onHover(TextActions.showText(Text.of("Redeem the forestry kit"))).style(TextStyles.BOLD).build();
-    public static Text EnchanterKitString = Text.builder("Redeem Immersive Kit").onClick(TextActions.runCommand("/start immersive")).color(TextColors.AQUA).onHover(TextActions.showText(Text.of("Redeem the Immersive kit"))).style(TextStyles.BOLD).build();
+    public static Text EnviroKitString = Text.builder("Redeem Tinkers Kit").onClick(TextActions.runCommand("/start tinkerskit")).color(TextColors.AQUA).onHover(TextActions.showText(Text.of("Redeem the Tinkers kit"))).style(TextStyles.BOLD).build();
+    public static Text RefinedKitString = Text.builder("Redeem AE Kit").onClick(TextActions.runCommand("/start aekit")).color(TextColors.AQUA).onHover(TextActions.showText(Text.of("Redeem the AE kit"))).style(TextStyles.BOLD).build();
+    public static Text ThermalKitString = Text.builder("Redeem Botania Kit").onClick(TextActions.runCommand("/start botaniakit")).color(TextColors.AQUA).onHover(TextActions.showText(Text.of("Redeem the Botania kit"))).style(TextStyles.BOLD).build();
+    public static Text XnetKitString = Text.builder("Redeem ThermalExpansion Kit").onClick(TextActions.runCommand("/start thermalkit")).color(TextColors.AQUA).onHover(TextActions.showText(Text.of("Redeem the Thermal Expansion kit"))).style(TextStyles.BOLD).build();
+    public static Text FarmerKitString = Text.builder("Redeem Forestry Kit").onClick(TextActions.runCommand("/start forestrykit")).color(TextColors.AQUA).onHover(TextActions.showText(Text.of("Redeem the forestry kit"))).style(TextStyles.BOLD).build();
+    public static Text EnchanterKitString = Text.builder("Redeem Immersive Kit").onClick(TextActions.runCommand("/start immersivekit")).color(TextColors.AQUA).onHover(TextActions.showText(Text.of("Redeem the Immersive kit"))).style(TextStyles.BOLD).build();
 
 }
